a01f4e13f9a0a089cfeb189df3a3ec3da39ed348
Matthias Lau <myclau@gmail.com>
2019-12-03 10:17:21 +0800

    Update awssh.sh

--- awssh.sh
+++ awssh.sh
@@ -2,15 +2,16 @@
 
 function awssh(){
 	tagname=${1}
-	username=${2-ec2-user}
-	pemname=${3-aws-default.pem}
+        profile=${2-default}
+        username=${3-ec2-user}
+        pemname=${4-aws-default.pem}
 
 	if [ -z "$tagname" ] ; then
 		echo "tagname cannot be empty"
 		return 1
 	fi
 
-	ipaddress=$(aws ec2 describe-instances --filter Name=tag:Name,Values="$tagname" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+	ipaddress=$(aws ec2 describe-instances --profile $profile --filter Name=tag:Name,Values="$tagname" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 	if [ "$ipaddress" = "" ] ; then
                echo "Host not found / No Public Address"
                return 1
